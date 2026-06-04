90abb26c132686423035466ffb561faa862b6217
henry12-masa <56143218+henry12-masa@users.noreply.github.com>
2026-06-04 21:08:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const quizInfo = {
     title: "ひっかけ問題",
     desc: "数字・例外・判例の誤文対策"
   }
-  gyouseihou-advanced: {
+  gyouseihouadvanced: {
     title: "行政法上級100問",
     desc: "行政法総論・行政手続法・不服審査法・行政事件訴訟法・国家賠償法"
   }
